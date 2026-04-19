810e2538443eacf0ecbf438b77362a9704e04b42
Victoriia Kyselova <vic4ast@gmail.com>
2026-04-19 17:09:34 +0300

    Корисні допоміжні функції Hyva JavaScript

---
+++ app/code/Aureatelabs/CustomQuote/Controller/Move/InQuote.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Aureatelabs\CustomQuote\Controller\Move;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+
+class InQuote extends Action
+{
+    public function execute()
+    {
+        echo "My new page";
+        exit;
+    }
+}
